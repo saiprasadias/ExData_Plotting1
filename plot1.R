ebbfffcd819807b03c7f24c9b0992728f868e743
saiprasadias <44631115+saiprasadias@users.noreply.github.com>
2018-12-09 11:28:43 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,29 @@
+## Read the Household Electricity consumptio data from 
+## the data file household.txt with seperators = ";"
+
+library(dplyr)
+a<-  read.csv("household.txt", header= TRUE, sep = ";")
+
+## dimensions of Data Table = 20,75,259*9
+## names(a)
+## [1] "Date"                  "Time"                  "Global_active_power"  
+## [4] "Global_reactive_power" "Voltage"               "Global_intensity"     
+## [7] "Sub_metering_1"        "Sub_metering_2"        "Sub_metering_3"  
+
+## Converting Date variable to Date/Time classes in R 
+## using the as.date() function.
+
+a$Date <- as.Date(a$Date, "%d/%m/%Y")
+
+## We will only be using data from the dates 2007-02-01 and 2007-02-02.
+a <- filter(a, (Date == "2007-02-01" | Date == "2007-02-02") )
+## converting Global Active Power from factor to numeric
+a$Global_active_power <- as.numeric(as.character(a$Global_active_power))
+
+## Constructing the Histogram and saving it to a png file "plot1.png" with a width of 480 pixels 
+## and a height of 480 pixels
+
+png(filename = 'plot1.png', width = 480, height = 480)
+hist(a$Global_active_power, xlab = "Global Active Power (kilowatts)", 
+     main = "Global Active Power", col = "red", breaks = 15)
+dev.off()
